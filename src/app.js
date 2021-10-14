98e7e29d9dc4dd8f0894aad475a6667b6fa06c1d
Marina Roz das Chagas <marinarozdaschagas@gmail.com>
2021-10-14 18:12:15 +0200

    3 step

--- src/app.js
+++ src/app.js
@@ -2,11 +2,33 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = () => {
-  let randomNumber = Math.random() * 10;
-  document.querySelector(`#the-excuse`).innerHTML = randomNumber;
-  console.log(`Hello Rigo from the console!` + randomNumber);
+  document.querySelector(`#the-excuse`).innerHTML = generatorexcuse();
+  console.log(`Hello Rigo from the console!`);
+};
+
+let generatorexcuse = () => {
+  let pronoun = [`the`, `a`, `my`];
+  let subjetc = [`dog`, +` cloud`, `friend`];
+  let action = [`took my`, `laugh at`, `burn my`];
+  let possesion = [`homework`, `toe`, `car`];
+  let where = [`in my place`, `at the park`, `at the kitchen`];
+
+  let pronaounIndex = 0;
+  let subjectIndex = 0;
+  let actionIndex = 0;
+  let possesionIndex = 0;
+  let whereIndex = 0;
+
+  return (
+    pronoun[pronaounIndex] +
+    ` ` +
+    subjetc[subjectIndex] +
+    ` ` +
+    action[actionIndex] +
+    ` ` +
+    possesion[possesionIndex] +
+    ` ` +
+    where[whereIndex]
+  );
 };
