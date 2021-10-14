1359105af838df8b63089ca8fe8293eb6783f828
Marina Roz das Chagas <marinarozdaschagas@gmail.com>
2021-10-14 18:42:08 +0200

    4 step

--- src/app.js
+++ src/app.js
@@ -3,22 +3,24 @@ import "bootstrap";
 import "./style.css";
 
 window.onload = () => {
-  document.querySelector(`#the-excuse`).innerHTML = generatorexcuse();
+  document.querySelector(`btn`).addEventlistener("click", () => {
+      document.querySelector(`#the-excuse`).innerHTML = generatorexcuse();
+    });
   console.log(`Hello Rigo from the console!`);
 };
 
 let generatorexcuse = () => {
-  let pronoun = [`the`, `a`, `my`];
+  let pronoum = [`the`, `a`, `my`];
   let subjetc = [`dog`, +` cloud`, `friend`];
   let action = [`took my`, `laugh at`, `burn my`];
   let possesion = [`homework`, `toe`, `car`];
   let where = [`in my place`, `at the park`, `at the kitchen`];
 
-  let pronaounIndex = 0;
-  let subjectIndex = 0;
-  let actionIndex = 0;
-  let possesionIndex = 0;
-  let whereIndex = 0;
+  let pronoumIndex = Math.floor(Math.random() * pronoum.length);
+  let subjectIndex = Math.floor(Math.random() * subjetc.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possesionIndex = Math.floor(Math.random() * possesion.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
 
   return (
     pronoun[pronaounIndex] +
